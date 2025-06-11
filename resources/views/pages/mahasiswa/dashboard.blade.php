2abf513ce6e934a82b7ce9de598023ff54c579a1
reniyasinta <reniyasinta6@example.com>
2025-06-11 23:49:35 +0800

    dashboard aja

--- resources/views/pages/mahasiswa/dashboard.blade.php
+++ resources/views/pages/mahasiswa/dashboard.blade.php
@@ -224,12 +224,9 @@
 </div>
 @endsection
 
-<<<<<<< HEAD
-=======
 @push('scripts')
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
 
 @endpush
->>>>>>> 0ed5b676f569c3925722bbc449304dd8725721ee
 
